06aa0395b2a79487c9e0e65d55d1e4aa66708f4a
krez274 <keeganperez@icloud.com>
2014-04-11 17:36:34 +0000

    We made a hash to check the users choice and see if they won or if the computer won

--- simple_rps.rb
+++ simple_rps.rb
@@ -1,4 +1,9 @@
-OUTCOMES ={"rock" => {"rock" => "tie", "Paper" => "player 2","scissors" => "player 1"},
-           "paper" => {},
-           "scissors" => {}
+OUTCOMES = {"rock" => {"rock" => "tie", "Paper" => "player 2","scissors" => "player 1"},
+  "paper" => {"paper" => "tie", "rock" => "player 1", "scissors" => "player 2"},
+  "scissors" => {"scissors" => "tie", "rock" => "player 2", "paper" => "player 1"}
            }
+
+computer_choice = ["rock", "paper", "scissors"].sample
+puts "Pick a choice,Rock,Paper or scissors?"
+user_choice = gets.chomp
+puts OUTCOMES[user_choice][computer_choice]
